fed78005bf03201871a32e8d63a3360aacf93464
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-25 09:23:59 +0700

    Update config.py

--- Confess/config.py
+++ Confess/config.py
@@ -6,4 +6,9 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
+API_ID = getenv("API_ID", "20211998")
+API_HASH = getenv("API_HASH", "beeeebe74c0c467c47c6ac4a1c9d75b5")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
+
+SESSION = getenv("SESSION", "")
+BOT_WORKERS = getenv("BOT_WORKERS", "4")
