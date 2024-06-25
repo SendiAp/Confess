e2af08c746ee4ee97cc4b2368bc76a00f23fdafb
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-25 09:19:09 +0700

    Update start.py

--- Confess/modules/start.py
+++ Confess/modules/start.py
@@ -6,3 +6,26 @@ from pyrogram.types import *
 
 from Confess.config import *
 from Confess import *
+
+@Bot.on_message(filters.command("start"))
+async def start_(client : Bot, message : Message):
+    quantity = 1
+    inp = message.text.split(None, 2)[1]
+    user = await client.get_chat(inp)
+    spam_text = ' '.join(message.command[2:])
+    quantity = int(quantity)
+
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+        for _ in range(quantity):
+            await message.reply_text("Message Sended Successfully !")
+            await bot.send_message(user.id, spam_text,
+                                      reply_to_messsge_id=reply_to_id)
+            await asyncio.sleep(0.15)
+        return
+
+    for _ in range(quantity):
+        await bot.send_message(user.id, spam_text)
+        await message.reply_text("Message Sended Successfully !")
+        await asyncio.sleep(0.15)
+      
