171bbe876ed4fa8bb58b4fad52ad8f68af97132d
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-26 12:41:34 +0700

    Update start.py

--- Confess/modules/start.py
+++ Confess/modules/start.py
@@ -9,7 +9,7 @@ from Confess.config import *
 from Confess import *
 
 @Bot.on_message(filters.command("confess"))
-async def confess(client : bot, message : Message):
+async def confess(client : User, message : Message):
     quantity = 1
     inp = message.text.split(None, 2)[1]
     user = await client.get_chat(inp)
@@ -20,12 +20,12 @@ async def confess(client : bot, message : Message):
         reply_to_id = message.reply_to_message.message_id
         for _ in range(quantity):
             await message.edit("Message Sended Successfully !")
-            await client.send_message(user.id, spam_text,
+            await bot.send_message(user.id, spam_text,
                                       reply_to_messsge_id=reply_to_id)
             await asyncio.sleep(0.15)
         return
 
     for _ in range(quantity):
-        await client.send_message(user.id, spam_text)
+        await bot.send_message(user.id, spam_text)
         await message.edit("Message Sended Successfully !")
         await asyncio.sleep(0.15)
