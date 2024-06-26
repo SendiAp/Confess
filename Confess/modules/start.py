e02768000f9a45dd630195c456bdefab115866be
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-26 20:57:38 +0700

    Update start.py

--- Confess/modules/start.py
+++ Confess/modules/start.py
@@ -10,22 +10,26 @@ from Confess import *
 
 @Bot.on_message(filters.command("confess"))
 async def confess(client : User, message : Message):
-    quantity = 1
-    inp = message.text.split(None, 2)[1]
-    user = await bot.get_chat(inp)
-    spam_text = ' '.join(message.command[2:])
-    quantity = int(quantity)
-
+    text = None
     if message.reply_to_message:
-        reply_to_id = message.reply_to_message.message_id
-        for _ in range(quantity):
-            await message.edit("Message Sended Successfully !")
-            await bot.send_message(user.id, spam_text,
-                                      reply_to_messsge_id=reply_to_id)
-            await asyncio.sleep(0.15)
-        return
+        user_id = message.reply_to_message.from_user.id
+    else:
+        text = message.text.split()
+        if len(text) < 2:
+            await message.reply_text("/send_text @username\n\nNB: Pastikan username valid dan terdaftar ditelegram (jangan gunakan username bot)")
+            return
+        username = text[1]
+        try:
+            user = await bot.get_chat(username)
+        except ValueError:
+            user = None
+        if user is None:
+            await message.reply_text(f"{username} tidak di temukan.")
+            return
+        user_id = user.id
 
-    for _ in range(quantity):
-        await bot.send_message(user.id, spam_text)
-        await message.edit("Message Sended Successfully !")
-        await asyncio.sleep(0.15)
+    message1 = await client.ask(message.chat.id, f"🤖 <b>Bot:</b> Berikan saya nama anda atau nama palsu atau bisa pake Anonymous. (5-10 karakter)", filters=filters.text)
+
+    message2 = await client.ask(message.chat.id, f"🤖 <b>Bot:</b> Berikan saya nama target yang ingin di confess. (5-10 karakter)", filters=filters.text)
+
+    message3 = await client.ask(message.chat.id, f"🤖 <b>Bot:</b> Berikan saya pesan untuk target. (5-500 karakter)", filters=filters.text)
