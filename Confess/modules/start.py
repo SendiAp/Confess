3dd3414db5e1d62005aaf82a304a384de8deda51
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-26 21:42:06 +0700

    Update start.py

--- Confess/modules/start.py
+++ Confess/modules/start.py
@@ -53,4 +53,35 @@ async def send_text(client : User, message : Message):
 
     try:
         await bot.send_message(user_id, CONFESS.format(('> message')), parse_mode=ParseMode.MARKDOWN)
-    
+        await message.reply("🏍️ 
+    except BaseException as e:
+        return await message.reply(f"`{e}`\n\nBuruan lapor @pikyus7")
+
+@Bot.on_message(filters.command("send_text"))
+async def send_text(client : User, message : Message):
+    text = None
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+    else:
+        text = message.text.split()
+        if len(text) < 2:
+            await message.reply_text("/send_text @username\n\nNB: Pastikan username valid dan terdaftar ditelegram (jangan gunakan username bot)")
+            return
+        username = text[1]
+        try:
+            user = await bot.get_chat(username)
+        except ValueError:
+            user = None
+        if user is None:
+            await message.reply_text(f"{username} tidak di temukan.")
+            return
+        user_id = user.id
+
+    await message.reply(f"🤜 Target Ditemukan {user.mention}")
+    message = await client.ask(message.chat.id, f"🤖 <b>Bot:</b> Silahkan tuliskan pesannya, Min 20Karakter", filters=filters.text)
+
+    try:
+        await bot.send_message(user_id, CONFESS.format(('> message')), parse_mode=ParseMode.MARKDOWN)
+    except BaseException as e:
+        return await message.reply(f"`{e}`\n\nBuruan lapor @pikyus7")
+            
