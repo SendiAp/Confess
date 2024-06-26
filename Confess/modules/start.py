9a81dbb054c87d72e6cd8b8e787dc7f8f2c0e21a
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-26 12:37:51 +0700

    Update start.py

--- Confess/modules/start.py
+++ Confess/modules/start.py
@@ -10,28 +10,22 @@ from Confess import *
 
 @Bot.on_message(filters.command("confess"))
 async def confess(client : bot, message : Message):
-    text = None
+    quantity = 1
+    inp = message.text.split(None, 2)[1]
+    user = await client.get_chat(inp)
+    spam_text = ' '.join(message.command[2:])
+    quantity = int(quantity)
+
     if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-    else:
-        text = message.text.split()
-        if len(text) < 2:
-            await message.reply_text("Maaf, format yang Anda berikan salah. Mohon balas ke pengguna atau berikan username/user ID.")
-            return
-        username = text[1]
-        try:
-            user = await client.get_chat(username)
-        except ValueError:
-            user = None
-        if user is None:
-            await message.reply_text(f"Maaf, pengguna {username} tidak ditemukan.")
-            return
-        user_id = user.id
+        reply_to_id = message.reply_to_message.message_id
+        for _ in range(quantity):
+            await message.edit("Message Sended Successfully !")
+            await client.send_message(user.id, spam_text,
+                                      reply_to_messsge_id=reply_to_id)
+            await asyncio.sleep(0.15)
+        return
 
-    users = str(user_id)
-    teks = await client.ask(message.chat.id, "Kirimkan saya teks", filters=filters.text)
-
-    try:
-        await bot.send_message(users, f"{teks.text}")
-    except BaseException as e:
-        return await message.reply(f"`{e}`\n\nBuruan lapor @pikyus7")
+    for _ in range(quantity):
+        await client.send_message(user.id, spam_text)
+        await message.edit("Message Sended Successfully !")
+        await asyncio.sleep(0.15)
