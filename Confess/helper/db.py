4c5f2030738fae2544f9f76045a2c82a8a3037b0
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-27 22:11:56 +0700

    Create db.py

---
+++ Confess/helper/db.py
@@ -0,0 +1,33 @@
+from pyrogram.types import *
+from typing import Dict, List, Union
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from config import *
+
+mongo_client = AsyncIOMotorClient(MONGO_DB_URL)
+db = mongo_client[DB_NAME]
+
+gcastdb = db['GCAST']
+
+#BROADCAST_USER
+async def get_gcast() -> list:
+    gcast = await gcastdb.find_one({"gcast_id": "gcast_id"})
+    if not gcast:
+        return []
+    return gcast["gcast"]
+
+async def add_gcast(user_id: int) -> bool:
+    gcast = await get_gcast()
+    gcast.append(user_id)
+    await gcastdb.update_one(
+        {"gcast_id": "gcast_id"}, {"$set": {"gcast": gcast}}, upsert=True
+    )
+    return True
+
+async def remove_gcast(user_id: int) -> bool:
+    gcast = await get_gcast()
+    gcast.remove(user_id)
+    await gcastdb.update_one(
+        {"gcast_id": "gcast_id"}, {"$set": {"gcast": gcast}}, upsert=True
+    )
+    return True
