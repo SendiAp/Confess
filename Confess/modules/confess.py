fbc80f188660272e4049a48280fc1a527df7d342
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-03 21:24:17 +0700

    Create confess.py

---
+++ Confess/modules/confess.py
@@ -0,0 +1,10 @@
+import asyncio
+import os
+
+from pyrogram import *
+from pyromod import listen
+from pyrogram.types import *
+
+from Confess.helper.db import *
+from Confess.config import *
+from Confess import *
