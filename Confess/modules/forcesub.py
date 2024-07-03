4a639e79064fd62c7147b161a42bdc7b7e6826be
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-03 21:22:13 +0700

    Create forcesub.py

---
+++ Confess/modules/forcesub.py
@@ -0,0 +1,5 @@
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from pyrogram.types import *
+from pyrogram import filters
+from Confess.config import *
+from Confess import *
