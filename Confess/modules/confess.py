c104406c77a4d9e85eb3210d66f8f45a259012ac
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-03 21:33:01 +0700

    Update confess.py

--- Confess/modules/confess.py
+++ Confess/modules/confess.py
@@ -8,3 +8,52 @@ from pyrogram.types import *
 from Confess.helper.db import *
 from Confess.config import *
 from Confess import *
+
+@Bot.on_message(filters.command("send_text"))
+async def send_text(client : User, message : Message):
+    text = None
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+    else:
+        text = message.text.split()
+        if len(text) < 2:
+            await message.reply_text("❌ Format Salah..!\nGunakan Format `/send_text @KangKurirConfess`")
+            return
+        username = text[1]
+        try:
+            user = await bot.get_chat(username)
+        except BaseException as e:
+            return await message.reply_text(f"`{e}`\n\nusername tidak di temukan.")
+
+    await message.reply(f"💌 <b>Confess</b> {username}")
+    message1 = await client.ask(message.chat.id, f"🤖 <b>Bot:</b> Kirimkan nama kamu, boleh dirahasiakan. (Maks 10 karakter)", filters=filters.text)
+    message1 = message1.text
+    line1 = len(message1)
+    
+    if line1 >= 10:
+        amount = len(message1)
+        return await message.reply(f"🤖 <b>Bot:</b> Nama terlalu panjang! Silahkan mulai dari awal {amount}/10")
+        
+    message2 = await client.ask(message.chat.id, f"🤖 <b>Bot:</b> Kirimkan nama {username} , bebas mau menamai dia apa. (Maks 10 karakter)", filters=filters.text)
+    message2 = message2.text
+    line2 = len(message2)
+    
+    if line2 >= 10:
+        amount = len(message2)
+        return await message.reply(f"🤖 <b>Bot:</b> Nama terlalu panjang! Silahkan mulai dari awal {amount}/10")
+        
+    message3 = await client.ask(message.chat.id, f"🤖 <b>Bot:</b> Silahkan tuliskan pesannya, (Minimal 20 Karakter)", filters=filters.text)
+    message3 = message3.text
+    line3 = len(message3)
+    
+    if 5 >= line3:
+        amount = len(message3)
+        return await message.reply(f"🤖 <b>Bot:</b> Pesan terlalu pendek! Silahkan mulai dari awal {amount}/5")
+        
+    try:
+        await bot.send_message(user.id, CONFESS.format(message1, message2, message3))
+        await message.reply(f"<b>✅ Your message has been successfully sent to</b> {username}")
+        await message.reply(ATTENTION, reply_markup=CLOSE)
+    except BaseException as e:
+        return await message.reply_text(f"`{e}`\n\nBuruan lapor @pikyus7")
+      
