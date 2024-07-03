0e5424d8cfeaf1b68c3a701f9fdb180666818c63
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-03 21:13:00 +0700

    Update db.py

--- Confess/helper/db.py
+++ Confess/helper/db.py
@@ -8,6 +8,7 @@ mongo_client = AsyncIOMotorClient(MONGO_DB_URL)
 db = mongo_client[DB_NAME]
 
 gcastdb = db['GCAST']
+blacklistdb = db['BLACKLIST']
 
 #BROADCAST_USER
 async def get_gcast() -> list:
@@ -31,3 +32,26 @@ async def remove_gcast(user_id: int) -> bool:
         {"gcast_id": "gcast_id"}, {"$set": {"gcast": gcast}}, upsert=True
     )
     return True
+    
+async def get_blacklist() -> list:
+    blacklist = await blacklistdb.find_one({"blacklist_id": "blacklist_id"})
+    if not blacklist:
+        return []
+    return blacklist["blacklist"]
+
+async def add_blacklist(user_id: int) -> bool:
+    blacklist = await get_blacklist()
+    blacklist.append(user_id)
+    await blacklistdb.update_one(
+        {"blacklist_id": "blacklist_id"}, {"$set": {"blacklist": blacklist}}, upsert=True
+    )
+    return True
+
+async def remove_blacklist(user_id: int) -> bool:
+    blacklist = await get_blacklist()
+    blacklist.remove(user_id)
+    await blacklistdb.update_one(
+        {"blacklist_id": "blacklist_id"}, {"$set": {"blacklist": blacklist}}, upsert=True
+    )
+    return True
+    
