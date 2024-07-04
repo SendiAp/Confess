9885e373a159d70509cf0255e56c919675518366
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-04 10:44:41 +0700

    Update buttons.py

--- Confess/modules/buttons.py
+++ Confess/modules/buttons.py
@@ -1,3 +1,5 @@
+from Confess.config import *
+
 START_BUTTONS = InlineKeyboardMarkup(
     [
         [
@@ -9,10 +11,10 @@ START_BUTTONS = InlineKeyboardMarkup(
         ],
         [
             InlineKeyboardButton("❓Bantuan", callback_data="close"),
-            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+            InlineKeyboardButton("♻️Price", callback_data="close"),
         ],
         [
-            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+            InlineKeyboardButton("Groups Support", url=f"t.me{FORCE_SUB_GROUP}"),
         ],
     ]
 )
