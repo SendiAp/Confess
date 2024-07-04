78eee85f8e1f4dd8f69c4613fc5931e50ace52cb
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-04 11:22:10 +0700

    Update start.py

--- Confess/modules/start.py
+++ Confess/modules/start.py
@@ -26,18 +26,6 @@ def broadcast(func):
         await func(client, message)
     return wrapper
 
-START_TEXT = """
-👋Hai {}!
-@{} Adalah <b>Bot yang akan membantu</b> Kalian mengirim menfess crush mu langsung.
-
-<b>👉Kurir Saya</b> @KangKurirMenfess Dia akan mengantarkan pesan menfess mu.
-
-<b>❓ APA SAJA PERINTAHNYA ❓</b>
-Tekan /help untuk melihat semua daftar perintah dan cara kerjanya.
-
-[📃Rules Confess](t.me/PTSMProject)
-"""
-
 @Bot.on_message(filters.command("start"))
 @broadcast
 async def start(client : Bot, message : Message):
