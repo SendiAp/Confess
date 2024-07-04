73038356c7985eb6073184ad206966702470fbb4
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-04 10:39:35 +0700

    Create buttons.py

---
+++ Confess/modules/buttons.py
@@ -0,0 +1,18 @@
+START_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+        ],
+        [
+            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+        ],
+        [
+            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+        ],
+        [
+            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+        ],
+    ]
+)
