7c16d3867ab3eb1ef08c1375b47d078af1f57234
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-04 10:46:16 +0700

    Update buttons.py

--- Confess/modules/buttons.py
+++ Confess/modules/buttons.py
@@ -3,15 +3,15 @@ from Confess.config import *
 START_BUTTONS = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("➕ COMMANDS ➕", callback_data="close"),
+            InlineKeyboardButton("➕ COMMANDS ➕", callback_data="perintah"),
         ],
         [
-            InlineKeyboardButton("💰Point", callback_data="close"),
-            InlineKeyboardButton("💰Topup", callback_data="close"),
+            InlineKeyboardButton("💰Point", callback_data="point"),
+            InlineKeyboardButton("💰Topup", callback_data="topup"),
         ],
         [
-            InlineKeyboardButton("❓Bantuan", callback_data="close"),
-            InlineKeyboardButton("🛒Shop", callback_data="close"),
+            InlineKeyboardButton("❓Bantuan", callback_data="help"),
+            InlineKeyboardButton("🛒Shop", callback_data="shop"),
         ],
         [
             InlineKeyboardButton("Groups Support", url=f"t.me{FORCE_SUB_GROUP}"),
