4c5389a308b47ee78bc01b45417491c74169bcee
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-04 10:45:32 +0700

    Update buttons.py

--- Confess/modules/buttons.py
+++ Confess/modules/buttons.py
@@ -11,7 +11,7 @@ START_BUTTONS = InlineKeyboardMarkup(
         ],
         [
             InlineKeyboardButton("❓Bantuan", callback_data="close"),
-            InlineKeyboardButton("♻️Price", callback_data="close"),
+            InlineKeyboardButton("🛒Shop", callback_data="close"),
         ],
         [
             InlineKeyboardButton("Groups Support", url=f"t.me{FORCE_SUB_GROUP}"),
