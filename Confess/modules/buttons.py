a97deaca2c76f0071227f9329fa6a189716b1abc
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-04 10:42:30 +0700

    Update buttons.py

--- Confess/modules/buttons.py
+++ Confess/modules/buttons.py
@@ -1,14 +1,14 @@
 START_BUTTONS = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+            InlineKeyboardButton("➕ COMMANDS ➕", callback_data="close"),
         ],
         [
-            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
-            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+            InlineKeyboardButton("💰Point", callback_data="close"),
+            InlineKeyboardButton("💰Topup", callback_data="close"),
         ],
         [
-            InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
+            InlineKeyboardButton("❓Bantuan", callback_data="close"),
             InlineKeyboardButton("Abaikan Pesan ini", callback_data="close"),
         ],
         [
