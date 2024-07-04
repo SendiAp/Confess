5131483299bd55094a9010f1580fea04de654700
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-07-04 11:31:01 +0700

    Update message.py

--- Confess/modules/message.py
+++ Confess/modules/message.py
@@ -1,1 +1,13 @@
+START_TEXT = """
+👋Hai {}!
+
+@{} Adalah <b>Bot yang akan membantu</b> Kalian mengirim menfess crush mu langsung.
+
+<b>👉Kurir Saya</b> @KangKurirMenfess Dia akan mengantarkan pesan menfess mu.
+
+<b>❓ APA SAJA PERINTAHNYA ❓</b>
+Tekan /help untuk melihat semua daftar perintah dan cara kerjanya.
+
+[📃Rules Confess](t.me/PTSMProject)
+"""
 
